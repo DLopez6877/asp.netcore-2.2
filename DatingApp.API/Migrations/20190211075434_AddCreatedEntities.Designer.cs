b04132802959d42a102fea370dd0a943958e0969
dlopez6877 <DLopez6877@gmail.com>
2019-02-11 00:33:52 -0800

    working on adding photos, got api side working. droppped database and made new migration to correct table relationships

--- DatingApp.API/Migrations/20190211075434_AddCreatedEntities.Designer.cs
+++ DatingApp.API/Migrations/20190211075434_AddCreatedEntities.Designer.cs
@@ -9,8 +9,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20190207052233_ExtendedUserClass")]
-    partial class ExtendedUserClass
+    [Migration("20190211075434_AddCreatedEntities")]
+    partial class AddCreatedEntities
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -29,6 +29,8 @@ namespace DatingApp.API.Migrations
 
                     b.Property<bool>("IsMain");
 
+                    b.Property<string>("PublicId");
+
                     b.Property<string>("Url");
 
                     b.Property<int>("UserId");
@@ -37,7 +39,7 @@ namespace DatingApp.API.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Photo");
+                    b.ToTable("Photos");
                 });
 
             modelBuilder.Entity("DatingApp.API.Models.User", b =>
