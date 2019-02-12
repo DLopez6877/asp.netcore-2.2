a350dc23cd784ba01ad5552aadc9e7a4397d20c0
dlopez6877 <DLopez6877@gmail.com>
2019-02-11 23:11:13 -0800

    allow users to update main photo. photo currently doesn't update on change in the nav bar. will fix next

--- DatingApp-SPA/src/app/services/auth.service.ts
+++ DatingApp-SPA/src/app/services/auth.service.ts
@@ -3,16 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
-
-  constructor(private http: HttpClient) {}
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
+
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -20,7 +22,9 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          localStorage.setItem('user', JSON.stringify(user.user));
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          this.currentUser = user.user;
         }
       })
     );
