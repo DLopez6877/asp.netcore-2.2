5f34046ef83757a979b4102011cfa0b66a3c0159
dlopez6877 <DLopez6877@gmail.com>
2019-02-13 01:40:38 -0800

    set main photo in authService as observable and have components subscribe to it for source of truth. update local storage on main photo change for persistance

--- DatingApp-SPA/src/app/services/auth.service.ts
+++ DatingApp-SPA/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
@@ -13,18 +14,24 @@ export class AuthService {
   jwtHelper = new JwtHelperService();
   decodedToken: any;
   currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
   constructor(private http: HttpClient) {}
 
+  changeMemberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl);
+  }
+
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
-      map((response: any) => {
-        const user = response;
-        if (user) {
-          localStorage.setItem('token', user.token);
-          localStorage.setItem('user', JSON.stringify(user.user));
-          this.decodedToken = this.jwtHelper.decodeToken(user.token);
-          this.currentUser = user.user;
+      map((loginResponse: any) => {
+        if (loginResponse) {
+          localStorage.setItem('token', loginResponse.token);
+          localStorage.setItem('user', JSON.stringify(loginResponse.user));
+          this.decodedToken = this.jwtHelper.decodeToken(loginResponse.token);
+          this.currentUser = loginResponse.user;
+          this.changeMemberPhoto(this.currentUser.photoUrl);
         }
       })
     );
