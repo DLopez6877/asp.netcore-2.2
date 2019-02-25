59a1c015e5811e2e57c1309561b935f6fad1279f
dlopez6877 <DLopez6877@gmail.com>
2019-02-24 23:03:59 -0800

    move stylesheets to assets folder. add theme to user, add migration, and update database

--- DatingApp.API/Dtos/UserForListDto.cs
+++ DatingApp.API/Dtos/UserForListDto.cs
@@ -5,6 +5,7 @@ namespace DatingApp.API.Dtos
     public class UserForListDto
     {
         public int Id { get; set; }
+        public string Theme { get; set; }
         public string Username { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
---
+++ DatingApp.API/Migrations/20190225065327_AddThemeToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddThemeToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Users");
+        }
+    }
+}
