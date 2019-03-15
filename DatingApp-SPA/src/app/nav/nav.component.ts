de87d4e60d580b4adf4649cc90ae31397e4abc53
dlopez6877 <DLopez6877@gmail.com>
2019-03-15 00:01:59 -0700

    remove bug that prevented people from logging in

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -26,10 +26,6 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    if (!this.model.valid) {
-      this.alertify.error('Username and password are required');
-      return;
-    }
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('logged in successfully');
       this.themeService.invokeThemeChange();
